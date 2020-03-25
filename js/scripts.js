fd62b59e8e4ca35d1e7f7e5c4aff26ea0c1b7d8e
djzevenbergen <djzevenbergen@gmail.com>
2020-03-25 10:55:14 -0700

    add modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,34 @@
 $(document).ready(function() { 
- $("#formOne").submit(function(event) {
+  var modal = document.getElementById("myModal");
+  var span = document.getElementById("close");
+  $("#formOne").submit(function(event) {
     var name = $("input#name").val();
     var about = $("input#about").val();
     var date = $("input#date").val();
     var time = $("input#time").val();
-    alert(name + about + date + time);
+    
+    
 
-    $(".info").append(name + "<br>" + about + "<br>" + date + "<br>" + time);
+    $(".info").append("<h2> Booking Successful! </h2>" + "<br>" + name + "<br>" + about + "<br>" + date + "<br>" + time);
+    
+    modal.style.display = "block";
+
     event.preventDefault();
+
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+  
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+  
 
 
   });
+
+
+  
 });
